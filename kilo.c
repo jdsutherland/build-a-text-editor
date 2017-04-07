3cf84e0a86d15a4449da3132b01860a617f8647d
Jeff Sutherland <jdsuther@asu.edu>
2017-04-07 13:35:04 +0700

    Add error handling

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -6,14 +7,20 @@
 
 struct termios orig_termios;
 
+void die(const char *s)
+{
+  perror(s);
+  exit(1);
+}
+
 void disable_raw_mode()
 {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 
 void enable_raw_mode()
 {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
   atexit(disable_raw_mode);
 
   struct termios raw = orig_termios;
@@ -23,7 +30,7 @@ void enable_raw_mode()
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VMIN] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main()
@@ -32,7 +39,7 @@ int main()
 
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
 
     if (iscntrl(c)) {
       printf("%d\r\n", c);
