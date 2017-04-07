47e61e9e6e8ef9a7450a58a1091a872cfefa3f68
Jeff Sutherland <jdsuther@asu.edu>
2017-04-07 12:32:19 +0700

    Add raw mode to hookup stdin
    By default, the terminal starts in canonical mode, only sending input on
    `Enter`.
    
    Additionally:
    * Turns off echoing
    * Restores previous terminal state on program exit.

--- kilo.c
+++ kilo.c
@@ -1,8 +1,33 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
+
+struct termios orig_termios;
+
+void disable_raw_mode()
+{
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enable_raw_mode()
+{
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disable_raw_mode);
+
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
+
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
   char c;
+
+  enable_raw_mode();
+
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
 }
